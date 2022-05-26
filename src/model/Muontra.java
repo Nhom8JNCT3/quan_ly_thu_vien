3a275528770a6d3479341f9230ddb61c4f8ac805
NgocTrongT <hoaganheo@gmail.com>
2022-05-26 14:17:09 +0700

    them muon tra

---
+++ src/model/Muontra.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Admin
+ */
+public class Muontra {
+    private String mamuon;
+    private String madg;
+    private String masach;
+    private String ngaymuon;
+    private String ngayhentra;
+    private String ngaytra;
+    
+    public String getMamuon(){
+        return mamuon;
+    }
+    
+    public void setMamuon(String mamuon){
+        this.mamuon = mamuon;
+    }
+        public String getMadg(){
+        return madg;
+    }
+    
+    public void setMadg(String madg){
+        this.madg = madg;
+    }
+        public String getMasach(){
+        return masach;
+    }
+    
+    public void setMasach(String masach){
+        this.masach = masach;
+    }
+    
+       public String getNgaymuon(){
+        return ngaymuon;
+    }
+    
+    public void setNgaymuon(String ngaymuon){
+        this.ngaymuon = ngaymuon;
+    } 
+        public String getNgayhentra(){
+        return ngayhentra;
+    }
+    
+    public void setNgayhentra(String ngayhentra){
+        this.ngayhentra = ngayhentra;
+    }
+     
+        public String getNgaytra(){
+        return ngaytra;
+    }
+    
+    public void setNgaytra(String ngaytra){
+        this.ngaytra = ngaytra;
+    }
+    
+    
+}
